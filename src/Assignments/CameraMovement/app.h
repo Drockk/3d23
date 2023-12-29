2934f8d23660c31d6d7099a57e497d517b9afefd
Bartosz Zielonka <batziell@gmail.com>
2023-12-29 10:10:22 +0100

    Added CameraMovement

---
+++ src/Assignments/CameraMovement/app.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vector>
+
+#include "Application/application.h"
+#include "Application/utils.h"
+
+#include "glad/gl.h"
+
+#include "Camera.h"
+#include "CameraControler.h"
+
+class SimpleShapeApplication : public xe::Application
+{
+public:
+    SimpleShapeApplication(const int width, const int height, const std::string& title, const bool debug):
+    Application(width, height, title, debug) {
+    }
+
+    void init() override;
+
+    void frame() override;
+
+    void framebuffer_resize_callback(int w, int h) override;
+    void scroll_callback(double xoffset, double yoffset) override;
+
+    void mouse_button_callback(int button, int action, int mods) override;
+    void cursor_position_callback(double x, double y) override;
+
+private:
+
+    GLuint vao_{};
+    GLuint m_uniform_vertex_buffer{};
+
+    std::vector<GLushort> m_indices;
+
+    Camera m_camera;
+    CameraControler m_camera_controler;
+};
